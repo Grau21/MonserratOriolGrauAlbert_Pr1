6b73730101f16e085c77ac99cf27122453c3034e
Grau21 <albertgrauperez@gmail.com>
2026-02-18 18:30:03 +0100

    Crear Parcela.java

---
+++ src/prog2/model/Parcela.java
@@ -0,0 +1,27 @@
+package prog2.model;
+
+public class Parcela extends Allotjament {
+    private float mida;
+    private boolean connexioElectrica;
+
+    public Parcela(String nom, String id, float mida, boolean connexioElectrica) {
+        super(nom, id, 4, 2);
+        this.mida = mida;
+        this.connexioElectrica = connexioElectrica;
+    }
+
+    public float getMida() { return mida; }
+    public void setMida(float mida) { this.mida = mida; }
+    public boolean isConnexioElectrica() { return connexioElectrica; }
+    public void setConnexioElectrica(boolean connexioElectrica) { this.connexioElectrica = connexioElectrica; }
+
+    @Override
+    public boolean correcteFuncionament() {
+        return connexioElectrica;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Parcela{mida=" + mida + ", connexioElectrica=" + connexioElectrica + "}";
+    }
+}
