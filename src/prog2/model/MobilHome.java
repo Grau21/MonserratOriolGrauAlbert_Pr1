1e3f1e844cb2eac272057f8757f6bb58c8155c9d
Grau21 <albertgrauperez@gmail.com>
2026-02-24 18:28:52 +0100

    Crear MobilHome.java

---
+++ src/prog2/model/MobilHome.java
@@ -0,0 +1,24 @@
+package prog2.model;
+
+public class MobilHome extends Casa {
+    private boolean terrassaBarbacoa;
+
+    public MobilHome(String nom, String id, String mida, int habitacions, int placesPersones,
+                     boolean terrassaBarbacoa, long estadaMinimaALTA, long estadaMinimaBAIXA) {
+        super(nom, id, estadaMinimaALTA, estadaMinimaBAIXA, mida, habitacions, placesPersones);
+        this.terrassaBarbacoa = terrassaBarbacoa;
+    }
+
+    public boolean isTerrassaBarbacoa() { return terrassaBarbacoa; }
+    public void setTerrassaBarbacoa(boolean terrassaBarbacoa) { this.terrassaBarbacoa = terrassaBarbacoa; }
+
+    @Override
+    public boolean correcteFuncionament() {
+        return terrassaBarbacoa;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " MobilHome{terrassaBarbacoa=" + terrassaBarbacoa + "}";
+    }
+}
