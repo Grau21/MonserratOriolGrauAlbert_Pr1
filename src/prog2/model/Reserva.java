3b1276f65a40517f5413eaa3fabad563045b2864
OctopusSquared <oriolmon@gmail.com>
2026-02-25 22:08:59 +0100

    Classes: Client i Reserva

---
+++ src/prog2/model/Reserva.java
@@ -0,0 +1,49 @@
+package prog2.model;
+
+import java.time.LocalDate;
+
+public class Reserva implements InReserva {
+    private Allotjament allotjament_;
+    private Client client_;
+    private LocalDate dateEntrada_;
+    private LocalDate dataSortida_;
+
+    public Reserva(Allotjament allotjament_, Client client_,
+                   LocalDate dateEntrada_, LocalDate dataSortida_) {
+        this.allotjament_ = allotjament_;
+        this.client_ = client_;
+        this.dateEntrada_ = dateEntrada_;
+        this.dataSortida_ = dataSortida_;
+    }
+
+    public Allotjament getAllotjament_() { return allotjament_; }
+
+    public Client getClient() { return client_; }
+
+    public LocalDate getDataEntrada() { return dateEntrada_; }
+
+    public LocalDate getDataSortida()  { return dataSortida_; }
+
+    public void setAllotjament_(Allotjament allotjament_) {
+        this.allotjament_ = allotjament_;
+    }
+
+    public void setClient(Client client_) {
+        this.client_ = client_;
+    }
+
+    public void setDataEntrada(LocalDate dataEntrada_) {
+        this.dateEntrada_ = dataEntrada_;
+    }
+
+    public void setDataSortida(LocalDate dataSortida_) {
+        this.dataSortida_ = dataSortida_;
+    }
+
+    @Override
+    public String toString() {
+        return "Allotjament=" + allotjament_ + ", client=" + client_ +
+                ", Data d'entrada: " + dateEntrada_ +
+                ", data de sortida: " + dataSortida_ + ".";
+    }
+}
