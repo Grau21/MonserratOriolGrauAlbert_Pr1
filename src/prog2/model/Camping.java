2a400c6f8b8d0ad8d2ca6ea17a9e13780e3a0ba1
OctopusSquared <oriolmon@gmail.com>
2026-03-02 20:39:00 +0100

    Camping (incomplet)

---
+++ src/prog2/model/Camping.java
@@ -0,0 +1,48 @@
+package prog2.model;
+
+import prog2.vista.ExcepcioReserva;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Camping implements InCamping {
+    private String nom;
+
+    //String getNom() { return nom;}
+
+    //LlistaReserves getLlistaReserves();
+
+    //ArrayList<Allotjament> getLlistaAllotjaments();
+
+    //ArrayList<Client> getLlistaClients();
+
+    //int getNumAllotjaments();
+
+    //int getNumReserves();
+
+    //int getNumClients();
+
+    //void afegirClient(String nom_, String dni_);
+
+    //void afegirParcela(String nom_, String idAllotjament_, float metres, boolean connexioElectrica);
+
+    /*void afegirBungalow(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones,
+                        int placesParquing, boolean terrassa, boolean tv, boolean aireFred);*/
+
+    /*void afegirBungalowPremium(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones,
+                               int placesParquing, boolean terrassa, boolean tv, boolean aireFred,
+                               boolean serveisExtra, String codiWifi);*/
+
+    /*void afegirGlamping(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones,
+                        String material, boolean casaMascota);*/
+
+    /*void afegirMobilHome(String nom_, String idAllotjament_, String mida, int habitacions, int placesPersones,
+                         boolean terrassaBarbacoa);*/
+
+    //void afegirReserva(String id_, String dni_, LocalDate dataEntrada, LocalDate dataSortida) throws ExcepcioReserva;
+
+    //int calculAllotjamentsOperatius();
+
+    //Allotjament getAllotjamentEstadaMesCurta(InAllotjament.Temp temp);
+
+}
