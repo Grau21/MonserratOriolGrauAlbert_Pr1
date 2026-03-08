e7b727f8a43ae818b2f168b5b324163fc31b17b3
Grau21 <albertgrauperez@gmail.com>
2026-03-08 20:50:17 +0100

    Comentaris MobilHome.java

--- src/prog2/model/MobilHome.java
+++ src/prog2/model/MobilHome.java
@@ -5,18 +5,18 @@ public class MobilHome extends Casa {
 
     public MobilHome(String nom, String id, String mida, int habitacions, int placesPersones,
                      boolean terrassaBarbacoa) {
-        super(nom, id, mida, habitacions, placesPersones);
+        super(nom, id, mida, habitacions, placesPersones); // Heretem de casa amb el super.
         this.terrassaBarbacoa = terrassaBarbacoa;
     }
 
     public boolean isTerrassaBarbacoa() { return terrassaBarbacoa; }
     public void setTerrassaBarbacoa(boolean terrassaBarbacoa) { this.terrassaBarbacoa = terrassaBarbacoa; }
-
+// Correcte Funcionament.
     @Override
     public boolean correcteFuncionament() {
         return terrassaBarbacoa;
     }
-
+// Que tñe MobilHome.
     @Override
     public String toString() {
         return super.toString() + " MobilHome{terrassaBarbacoa=" + terrassaBarbacoa + "}";
