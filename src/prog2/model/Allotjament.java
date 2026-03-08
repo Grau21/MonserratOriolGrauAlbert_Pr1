8dba10e3b3d7a8d2c9d3f4205bd03ccd42a79f20
OctopusSquared <oriolmon@gmail.com>
2026-03-08 11:04:19 +0100

    Modificació: ara estadaMinima ALTA i BAIXA són variables estàtiques, és a dir, son la matixa per a tots els objectes de la classe Allotjament. També ens és útil per tema herències.

--- src/prog2/model/Allotjament.java
+++ src/prog2/model/Allotjament.java
@@ -3,14 +3,14 @@ package prog2.model;
 public abstract class Allotjament implements InAllotjament {
     private String nom;
     private String id;
-    private long estadaMinimaALTA_;
-    private long estadaMinimaBAIXA_;
+    private static long estadaMinimaALTA_;
+    private static long estadaMinimaBAIXA_;
 
     public Allotjament(String nom, String id, long estadaMinimaALTA_, long estadaMinimaBAIXA_) {
         this.nom = nom;
         this.id = id;
-        this.estadaMinimaALTA_ = estadaMinimaALTA_;
-        this.estadaMinimaBAIXA_ = estadaMinimaBAIXA_;
+        Allotjament.estadaMinimaALTA_ = estadaMinimaALTA_;
+        Allotjament.estadaMinimaBAIXA_ = estadaMinimaBAIXA_;
     }
 
     @Override
@@ -28,16 +28,18 @@ public abstract class Allotjament implements InAllotjament {
     @Override
     public long getEstadaMinima(Temp temp) {
         switch (temp) {
-            case ALTA: return estadaMinimaALTA_; break;
-            case BAIXA: return estadaMinimaBAIXA_; break;
+            case ALTA: return Allotjament.estadaMinimaALTA_;
+            case BAIXA: return Allotjament.estadaMinimaBAIXA_;
+            // default: throw new IllegalArgumentException("Temp no vàlid");
         }
-    return -1; // Revisar//////////////////////////////////////////
+    // Error, no s'hauria d'arribar mai a aquesta linia de codi
+    return -1;
     }
 
     @Override
     public void setEstadaMinima(long estadaMinimaALTA_, long estadaMinimaBAIXA_) {
-        this.estadaMinimaALTA_ = estadaMinimaALTA_;
-        this.estadaMinimaBAIXA_ = estadaMinimaBAIXA_;
+        Allotjament.estadaMinimaALTA_ = estadaMinimaALTA_;
+        Allotjament.estadaMinimaBAIXA_ = estadaMinimaBAIXA_;
     }
 
     @Override
